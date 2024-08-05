a93b62cfac07c464a34b690e314bd336bece6bf6
BrianNgQ <146939700+BrianNgQ@users.noreply.github.com>
2024-08-05 13:19:55 +0700

    ccc

--- BlazorApp1/Models/Courses.cs
+++ BlazorApp1/Models/Courses.cs
@@ -11,7 +11,7 @@ namespace BlazorApp1.Models
         public int SemesterId { get; set; }
         public string LectureId { get; set; }
         public int SubjectId { get; set; }
-
+        //xnxx//
         public Semesters Semester { get; set; }
         public ApplicationUser Lecture { get; set; }
         public Subjects Subject { get; set; }
