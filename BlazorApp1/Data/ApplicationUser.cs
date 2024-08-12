f2be15904f0a37190195b1028672a74a823a217c
Lộc Tô Bảo <tobaoloc5a4@gmail.com>
2024-08-11 23:35:07 -0700

    Added Student Manage

--- BlazorApp1/Data/ApplicationUser.cs
+++ BlazorApp1/Data/ApplicationUser.cs
@@ -7,6 +7,8 @@ namespace BlazorApp1.Data
     {
         public string Name { get; set; }
         public string Code { get; set; }
+        public string Role { get; set; }
+        //role
     }
 
 }
---
+++ BlazorApp1/Data/Migrations/20240812062511_Students.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Students : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "AspNetUsers");
+        }
+    }
+}
